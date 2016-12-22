b4a2579ccc149b3021aae9e644bb2d0df12cb503
Tayfun Ates <tayfunatesceng@gmail.com>
2016-12-22 21:27:22 +0300

    HKS Extraction parameters are added, Eigen include dependency is added to the feature extraction tool

--- TAFeatureExtraction/include/HKSDescExtraction.h
+++ TAFeatureExtraction/include/HKSDescExtraction.h
@@ -2,23 +2,78 @@
 #define HEAT_KERNEÞ_SIGNATURE_DESC_EXTRACTION_H
 
 #include "TAFeatureExtraction.h"
+#include "Eigen/SparseCore"
 
 namespace TAFeaExt
 {
 	//Heat Kernel Signature for a single vertex in a mesh
 	//Technical Details of the feature is described in http://www.lix.polytechnique.fr/~maks/papers/hks.pdf
+	//[Currently] Time interval is decided automatically on default from the eigen values as proposed in the paper.
+	//Set time interval values in case you do not want the default calculated ones
 	//HKSDescExtraction is a PerVertexFeatureExtraction
 	class HKSDescExtraction : public PerVertexFeatureExtraction
 	{
-	private:
-
 	public:
+
+		enum TypeOfLaplacian
+		{
+			STAR_LAPLACIAN = 1 //< http://www.cs.princeton.edu/courses/archive/fall10/cos526/papers/sorkine05.pdf
+		};
+
 		virtual ~HKSDescExtraction();
 		HKSDescExtraction();
 		HKSDescExtraction(const HKSDescExtraction& other);
 
 		virtual Result extract(PolygonMesh *mesh, std::vector<LocalFeaturePtr>& outFeatures);
 		virtual Result extract(PolygonMesh *mesh, const int& id, LocalFeaturePtr& outFeaturePtr);
+
+		/**
+		* @brief	Create the laplacian matrix from the polygon mesh according to TypeOfLaplacian and saves it into the object
+		*			[Currently] Only STAR Laplacian is supported
+		* @param	mesh Polygonial mesh from which the laplacian matrix will be created
+		* @param	typeLap Type of the laplacian matrix to be extracted
+		* @return	TACORE_OK if everything goes fine.
+		*/
+		Result createLaplacianMatrix(PolygonMesh *mesh, const TypeOfLaplacian& typeLap);
+
+		/**
+		* @brief	Clears the object by setting default parameters and deleting heap space
+		* @return	void
+		*/
+		void clear();
+
+		//Setters
+		void setTypeOfLaplacian(const TypeOfLaplacian& typeLap);
+		void setNumberOfEigenVals(const unsigned int& noEigenVals);
+		void setMinTimeVal(const float& minTime);
+		void setMaxTimeVal(const float& maxTime);
+		void setNumberOfTimeSamples(const unsigned int& noTimeSamples);
+
+		//Getters
+		TypeOfLaplacian getTypeOfLaplacian() const;
+		unsigned int getNumberOfEigenVals() const;
+		float getMinTimeVal() const;
+		float getMaxTimeVal() const;
+		unsigned int getNumberOfTimeSamples() const;
+
+	private:
+
+		TypeOfLaplacian m_LaplacianUsed; //< Type of the laplacian used in the extraction
+		unsigned int m_nNoEigenVal; //< Number of eigen vals and vects extracted from the laplacian matrix
+		float m_fTMin; //< Minimum value of the time interval
+		float m_fTMax; //< Maximum value of the time interval
+		unsigned int m_nNumberOfTimeSamples; //< Number of time values in the interval
+
+		Eigen::SparseMatrix<float> *m_mLaplacian;	// < Laplacian matrix, it is a class member because multiple executions for different time intervals should be enabled without re-construction.
+
+		/**
+		* @brief	Create the STAR laplacian matrix from the polygon mesh saves it into the object
+		*			[Currently] Only Triangular mesh is supported
+		* @param	mesh Polygonial mesh from which the laplacian matrix will be created
+		* @return	TACORE_OK if everything goes fine.
+		*/
+		Result createStarLaplacianMatrix(PolygonMesh *mesh);
+
 	};
 }
 
--- TAFeatureExtraction/src/HKSDescExtraction.cpp
+++ TAFeatureExtraction/src/HKSDescExtraction.cpp
@@ -5,17 +5,41 @@ namespace TAFeaExt
 {
 	HKSDescExtraction::~HKSDescExtraction()
 	{
-
+		clear();
 	}
 
 	HKSDescExtraction::HKSDescExtraction()
 	{
-
+		this->m_LaplacianUsed = STAR_LAPLACIAN;
+		this->m_nNoEigenVal = 300;
+		this->m_fTMin = -1.0f; //If not set from the user, this will be calculated by the extractor
+		this->m_fTMax = -1.0f; //If not set from the user, this will be calculated by the extractor
+		this->m_nNumberOfTimeSamples = 100;
+		this->m_mLaplacian = NULL;
 	}
 
 	HKSDescExtraction::HKSDescExtraction(const HKSDescExtraction& other)
 	{
+		this->m_LaplacianUsed = other.m_LaplacianUsed;
+		this->m_nNoEigenVal = other.m_nNoEigenVal;
+		this->m_fTMin = other.m_fTMin;
+		this->m_fTMax = other.m_fTMax;
+		this->m_nNumberOfTimeSamples = other.m_nNumberOfTimeSamples;
+		this->m_mLaplacian = other.m_mLaplacian; //Shallow copy
+	}
 
+	void HKSDescExtraction::clear()
+	{
+		this->m_LaplacianUsed = STAR_LAPLACIAN;
+		this->m_nNoEigenVal = 300;
+		this->m_fTMin = -1.0f; //If not set from the user, this will be calculated by the extractor
+		this->m_fTMax = -1.0f; //If not set from the user, this will be calculated by the extractor
+		this->m_nNumberOfTimeSamples = 100;
+		if (this->m_mLaplacian != NULL)
+		{
+			delete this->m_mLaplacian;
+			this->m_mLaplacian = NULL;
+		}
 	}
 
 	Result HKSDescExtraction::extract(PolygonMesh *mesh, std::vector<LocalFeaturePtr>& outFeatures)
@@ -36,5 +60,84 @@ namespace TAFeaExt
 		TriangularMesh *triMesh = (TriangularMesh*)mesh;
 
 		return TACORE_OK;
+	}
+
+	void HKSDescExtraction::setTypeOfLaplacian(const HKSDescExtraction::TypeOfLaplacian& typeLap)
+	{
+		this->m_LaplacianUsed = typeLap;
+	}
+
+	HKSDescExtraction::TypeOfLaplacian HKSDescExtraction::getTypeOfLaplacian() const
+	{
+		return this->m_LaplacianUsed;
+	}
+
+	void HKSDescExtraction::setNumberOfEigenVals(const unsigned int& noEigenVals)
+	{
+		this->m_nNoEigenVal = noEigenVals;
+	}
+
+	unsigned int HKSDescExtraction::getNumberOfEigenVals() const
+	{
+		return this->m_nNoEigenVal;
+	}
+
+	void HKSDescExtraction::setMinTimeVal(const float& minTime)
+	{
+		this->m_fTMin = minTime;
+	}
+
+	float HKSDescExtraction::getMinTimeVal() const
+	{
+		return this->m_fTMin;
+	}
+
+	void HKSDescExtraction::setMaxTimeVal(const float& maxTime)
+	{
+		this->m_fTMax = maxTime;
+	}
+
+	float HKSDescExtraction::getMaxTimeVal() const
+	{
+		return this->m_fTMax;
+	}
+
+	void HKSDescExtraction::setNumberOfTimeSamples(const unsigned int& noTimeSamples)
+	{
+		this->m_nNumberOfTimeSamples = noTimeSamples;
+	}
+
+	unsigned int HKSDescExtraction::getNumberOfTimeSamples() const
+	{
+		return this->m_nNumberOfTimeSamples;
+	}
+
+	Result HKSDescExtraction::createLaplacianMatrix(PolygonMesh *mesh, const TypeOfLaplacian& typeLap)
+	{
+		Result res = TACore::TACORE_OK;
+		if (typeLap == HKSDescExtraction::STAR_LAPLACIAN)
+		{
+			res = createStarLaplacianMatrix(mesh);
+		}
+		return res;
+	}
+
+	Result HKSDescExtraction::createStarLaplacianMatrix(PolygonMesh *mesh)
+	{
+		if (!mesh || mesh->getPolygonType() != TA_TRIANGULAR)
+		{
+			return TACORE_BAD_ARGS;
+		}
+
+		//We, now, know that the mesh is a triangular mesh
+		TriangularMesh *triMesh = (TriangularMesh*)mesh;
+
+		if (this->m_mLaplacian != NULL)
+		{
+			delete this->m_mLaplacian;
+		}
+
+		const size_t numberOfVertices = triMesh->verts.size();
+		this->m_mLaplacian = new Eigen::SparseMatrix<float>(numberOfVertices, numberOfVertices);
 	}
 }
