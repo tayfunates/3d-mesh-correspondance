1239264e6be1dbd2b9a8cde28180697e6e035bde
Tayfun Ates <tayfunatesceng@gmail.com>
2016-12-25 21:15:11 +0300

    main now contains two applications, one for HKS, one for wave desc. armadillo dependency is added to the application's project file

--- TAFeatureExtraction/src/main.cpp
+++ TAFeatureExtraction/src/main.cpp
@@ -8,12 +8,12 @@
 //Descriptors
 #include "AvgGeodesicDistance.h"
 #include "IntrinsicWaveDesc.h"
+#include "HeatKernelSignatureDesc.h"
 
 //Extractors
 #include "OnEdgeAvgGeoDistExtraction.h"
 #include "IntrinsicWaveDescExtraction.h"
-
-
+#include "HKSDescExtraction.h"
 
 using namespace TACore;
 using namespace TAShape;
@@ -28,7 +28,7 @@ int mainRet(int ret, std::string message)
 	return ret;
 }
 
-int main(int argc, char* argv[])
+int HKSExtractionTestAPP(int argc, char* argv[])
 {
 	// Parse args
 	TACore::ArgParser parser("TAShapeTest", "Loads, Saves, Shows different types of shapes");
@@ -42,27 +42,76 @@ int main(int argc, char* argv[])
 
 	std::string inputFile = parser.get("input");
 	std::string outputFile = parser.get("output");
-	 
+
 	ThreeDimOIShape mesh;
 	if (mesh.load(inputFile.c_str()) != TACORE_OK)
 	{
 		return mainRet(1, "Mesh cannot be loaded correctly");
 	}
 
-	TriangularMesh* triMesh = (TriangularMesh*) (mesh.getShape());
+	TriangularMesh* triMesh = (TriangularMesh*)(mesh.getShape());
+
+	TAFeaExt::HKSDescExtraction hksExtractor;
+	hksExtractor.setNumberOfEigenVals(50);
+	hksExtractor.setMinTimeVal(5.0f);
+	hksExtractor.setMaxTimeVal(15.0f);
+
+	std::vector<double> distances1;
+	std::vector<LocalFeaturePtr> feas1;
+	hksExtractor.extract(triMesh, feas1);
+
+	HeatKernelSignatureDesc* hksV0 = (HeatKernelSignatureDesc*)(feas1[0].get());
+	for (size_t v = 0; v < feas1.size(); v++)
+	{
+		HeatKernelSignatureDesc* hksVV = (HeatKernelSignatureDesc*)(feas1[v].get());
+		const double l2Distance = HeatKernelSignatureDesc::L2Distance(*hksVV, *hksV0);
+		distances1.push_back(log(1.0+l2Distance));
+	}
+
+	const double maxDistance1 = *std::max_element(distances1.begin(), distances1.end());
+	const double minDistance1 = *std::min_element(distances1.begin(), distances1.end());
+	std::cout << minDistance1 << " " <<  maxDistance1 << std::endl;
+	for (size_t i = 0; i < distances1.size(); i++)
+	{
+		distances1[i] = distances1[i] / maxDistance1;
+	}
+
+	mesh.showVertexColors(distances1);
+	return mainRet(1, "Main Test Successfully Ended");
+}
+
+int IntrinsicWaveExtractionTestAPP(int argc, char* argv[])
+{
+	// Parse args
+	TACore::ArgParser parser("TAShapeTest", "Loads, Saves, Shows different types of shapes");
+	parser.addArg("input", "", true, 1, "", "Input shape file to be loaded");
+	parser.addArg("output", "", true, 1, "", "Output shape file to be saved");
+
+	if (!parser.parseCommandLine(argc, argv))
+	{
+		mainRet(-1, "Command line parameters cannot be parsed correctly");
+	}
+
+	std::string inputFile = parser.get("input");
+	std::string outputFile = parser.get("output");
+
+	ThreeDimOIShape mesh;
+	if (mesh.load(inputFile.c_str()) != TACORE_OK)
+	{
+		return mainRet(1, "Mesh cannot be loaded correctly");
+	}
+
+	TriangularMesh* triMesh = (TriangularMesh*)(mesh.getShape());
 
 	TAFeaExt::IntrinsicWaveDescExtraction waveDescExtractor;
 	waveDescExtractor.setMaxGeodesicRadius(1000.0f);
 	waveDescExtractor.setNumberOfWavesSampled(10);
 
-	//TACore::Timer timer;
-	//std::cout << "Geodesic Distances for all vertices are being calculated" << std::endl;
 
 	for (size_t v = 0; v < triMesh->verts.size(); v++)
 	{
 		LocalFeaturePtr localFeaturePtr;
 		waveDescExtractor.extract(triMesh, v, localFeaturePtr);
-		//std::cout << "Average geodesic distance of vertex: " << v << " is " << ((AvgGeodesicDistance*)(localFeaturePtr.get()))->m_distance << std::endl;
 
 		break;
 	}
@@ -70,4 +119,10 @@ int main(int argc, char* argv[])
 	mesh.showClosedEdges(waveDescExtractor.waves);
 
 	return mainRet(1, "Main Test Successfully Ended");
+}
+
+int main(int argc, char* argv[])
+{
+	/*return IntrinsicWaveExtractionTestAPP(argc, argv);*/
+	return HKSExtractionTestAPP(argc, argv);
 }
